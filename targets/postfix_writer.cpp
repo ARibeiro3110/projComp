79ebac3dc1acbb69626e3e6f1f9241ddf39fe3af
Afonso Ribeiro <afonsodaconceicaoribeiro@tecnico.ulisboa.pt>
2024-05-31 14:07:27 +0100

    postfix_writer: fix and node, or node; integer node, double node, string node

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -103,11 +103,11 @@ void til::postfix_writer::do_not_node(cdk::not_node * const node, int lvl) {
 void til::postfix_writer::do_and_node(cdk::and_node * const node, int lvl) {
   ASSERT_SAFE_EXPRESSIONS;
 
-  int lbl = ++_lbl;
+  int lbl;
   node->left()->accept(this, lvl + 2);
   _pf.DUP32();
-  _pf.JZ(mklbl(lbl));
-  node->right()->accept(this, lvl + 2);
+  _pf.JZ(mklbl(lbl = ++_lbl));
+  node->right()->accept(this, lvl);
   _pf.AND();
   _pf.ALIGN();
   _pf.LABEL(mklbl(lbl));
@@ -115,11 +115,11 @@ void til::postfix_writer::do_and_node(cdk::and_node * const node, int lvl) {
 void til::postfix_writer::do_or_node(cdk::or_node * const node, int lvl) {
   ASSERT_SAFE_EXPRESSIONS;
 
-  int lbl = ++ _lbl;
+  int lbl;
   node->left()->accept(this, lvl + 2);
   _pf.DUP32();
-  _pf.JNZ(mklbl(lbl));
-  node->right()->accept(this, lvl + 2);
+  _pf.JNZ(mklbl(lbl = ++ _lbl));
+  node->right()->accept(this, lvl);
   _pf.OR();
   _pf.ALIGN();
   _pf.LABEL(mklbl(lbl));
@@ -136,26 +136,38 @@ void til::postfix_writer::do_sequence_node(cdk::sequence_node * const node, int 
 //---------------------------------------------------------------------------
 
 void til::postfix_writer::do_integer_node(cdk::integer_node * const node, int lvl) {
-  _pf.INT(node->value()); // push an integer
+  if (inFunction()) {
+    _pf.INT(node->value()); // push an integer
+  } else {
+    _pf.SINT(node->value());
+  }
 }
 
 
 void til::postfix_writer::do_double_node(cdk::double_node * const node, int lvl) {
-  _pf.DOUBLE(node->value()); // push a double
+  if (inFunction()) {
+    _pf.DOUBLE(node->value());
+  } else {
+    _pf.SDOUBLE(node->value());
+  }
 }
 
 void til::postfix_writer::do_string_node(cdk::string_node * const node, int lvl) {
-  int lbl1;
+  int lbl;
 
   /* generate the string */
   _pf.RODATA(); // strings are DATA readonly
   _pf.ALIGN(); // make sure we are aligned
-  _pf.LABEL(mklbl(lbl1 = ++_lbl)); // give the string a name
+  _pf.LABEL(mklbl(lbl = ++_lbl)); // give the string a name
   _pf.SSTRING(node->value()); // output string characters
 
-  /* leave the address on the stack */
-  _pf.TEXT(); // return to the TEXT segment
-  _pf.ADDR(mklbl(lbl1)); // the string to be printed
+  if (inFunction()) {
+    _pf.TEXT(_functionLabels.top());
+    _pf.ADDR(mklbl(lbl));
+  } else {
+    _pf.DATA();
+    _pf.SADDR(mklbl(lbl));
+  }
 }
 
 //---------------------------------------------------------------------------
